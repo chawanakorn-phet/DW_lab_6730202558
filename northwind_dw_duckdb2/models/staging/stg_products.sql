f96c197ff46f0f4dd557a1a3eb7cfc50bc799901
chawanakorn-phet <chawanakron.p@kkumail.com>
2026-07-22 03:49:43 +0000

    ?

--- northwind_dw_duckdb2/models/staging/stg_products.sql
+++ northwind_dw_duckdb2/models/staging/stg_products.sql
@@ -1,17 +1,8 @@
 with source as (
-    select *
-    from {{ source('northwind', 'products') }}
-)
 
+    select * from {{ source('northwind', 'products') }}
+)
 select
-    product_id,
-    product_name,
-    supplier_id,
-    category_id,
-    quantity_per_unit,
-    unit_price,
-    units_in_stock,
-    units_on_order,
-    reorder_level,
-    discontinued
+    *,
+    current_localtimestamp() as ingestion_timestamp
 from source
